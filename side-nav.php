75b855cbed9d033b4c3438b8991778479fa15a24
kazu_kkk <hirose.kazunori.ard@gmail.com>
2026-02-06 01:10:36 +0900

    サイドナビゲーションを更新し、不要な.DS_Storeファイルを修正しました。

--- side-nav.php
+++ side-nav.php
@@ -1,24 +1,6 @@
 <aside id="custom-side-nav" class="widget-area">
 
 
-    <!-- カテゴリ一覧 -->
-    <h2 class="widget-title">カテゴリ</h2>
-    <ul>
-        <?php
-        // 除外するカテゴリIDを指定
-        $exclude_ids = array(2, 4, 5); // 除外したいカテゴリのID
-
-        // カテゴリを取得してリスト表示（除外指定を追加）
-        $categories = get_categories(array(
-            'exclude' => $exclude_ids
-        ));
-        foreach ($categories as $category) {
-            echo '<li><a href="' . esc_url(get_category_link($category->term_id)) . '">' . esc_html($category->name) . '</a></li>';
-        }
-        ?>
-    </ul>
-
-
     <!-- プロフィールカード -->
     <div class="profile-card">
         <?php
@@ -36,4 +18,24 @@
             <a href="https://note.com" target="_blank" class="social-button"><img src="<?php echo get_template_directory_uri(); ?>/images/note-icon.png" alt="Note"></a>
         </div> -->
     </div>
+
+
+    <!-- カテゴリ一覧 -->
+    <h2 class="widget-title">カテゴリ</h2>
+    <ul>
+        <?php
+        // 除外するカテゴリIDを指定
+        $exclude_ids = array(2, 4, 5); // 除外したいカテゴリのID
+
+        // カテゴリを取得してリスト表示（除外指定を追加）
+        $categories = get_categories(array(
+            'exclude' => $exclude_ids,
+            'orderby' => 'term_order', // Category Order and Taxonomy Terms Order用
+            'order'   => 'ASC',
+        ));
+        foreach ($categories as $category) {
+            echo '<li><a href="' . esc_url(get_category_link($category->term_id)) . '">' . esc_html($category->name) . '</a></li>';
+        }
+        ?>
+    </ul>
 </aside>
